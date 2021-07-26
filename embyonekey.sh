4c7d038b89dc2a225ddfd867c88a25aaa61bd730
zliuzz <42171074+zliuzz@users.noreply.github.com>
2021-07-26 17:12:14 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /etc/nginx/sites-enabled
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/proxy.conf
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
+wget https://raw.githubusercontent.com/zliuzz/embyonekey/master/proxy.conf
+curl https://raw.githubusercontent.com/zliuzz/embyonekey/master/guomi.cer >> /etc/ssl/certs/ca-certificates.crt
 cd /var/packages/EmbyServer/target/mono/bin
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /volume1/web/mb3admin.com
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.key.pem
+wget https://raw.githubusercontent.com/zliuzz/embyonekey/master/mb3admin.com.cert.pem
+wget https://raw.githubusercontent.com/zliuzz/embyonekey/master/mb3admin.com.key.pem
 cat mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/zliuzz/embyonekey/master/user.conf
 nginx -s reload
 done
